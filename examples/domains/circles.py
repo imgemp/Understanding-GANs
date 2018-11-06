Updated notebook + circle loc normalization was wrong

examples/domains/circles.py
@@ -71,8 +71,8 @@ class Circles(Data):
             image, label = result  # label = ('circle', (px, py, radius))
             image = (255-image)/255.
             px, py, radius = np.array(label[0][1])
-            px = (255-px)/255.
-            py = (255-py)/255.
+            px = (px-dim-1)/float(dim)
+            py = (py-dim-1)/float(dim)
             radius = 0.5*(2*radius-min_size)/(max_size-min_size)
             label = np.array([px,py,radius])
             samples += [np.concatenate([image.flatten(), label]).astype('float32')]