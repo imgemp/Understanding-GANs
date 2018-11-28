419373943ccd5763552b97326e775bbaf529fce5
Ian Gemp <imgemp@gmail.com>
2018-11-28 13:19:55 -0500

    changing upsampling to nearestneighbor instead of bilinear

--- examples/domains/circles_small2.py
+++ examples/domains/circles_small2.py
@@ -307,31 +307,31 @@ class Generator(Net):
         self.main = nn.Sequential(
             # input is Z, going into a convolution: (c*hin - kernel)/stride + 1, c=4,stride=kernel=2 --> 2*hin
             # or (2*hin + 2*padding - kernel)/stride + 1, 2*hin + 2*padding-kernel+1, padding=1, kernel=3
-            nn.Upsample(scale_factor = 3, mode='bilinear'),
+            nn.functional.interpolate(scale_factor = 3, mode='nearest'),
             # nn.ReflectionPad2d(1),
             nn.Conv2d(input_dim, output_dim * 8, kernel_size=2, stride=1, padding=1),
             nn.BatchNorm2d(output_dim * 8),
             nn.ReLU(True),
             # state size. (output_dim*8) x 4 x 4
-            nn.Upsample(scale_factor = 2, mode='bilinear'),
+            nn.functional.interpolate(scale_factor = 2, mode='nearest'),
             # nn.ReflectionPad2d(1),
             nn.Conv2d(output_dim * 8, output_dim * 4, kernel_size=3, stride=1, padding=1, bias=True),
             nn.BatchNorm2d(output_dim * 4),
             nn.ReLU(True), # hin - kernel + 1 + 2*padding (assumes stride=1)   3*hin - 2 + 1 + 2 = 
             # state size. (output_dim*4) x 8 x 8
-            nn.Upsample(scale_factor = 2, mode='bilinear'),
+            nn.functional.interpolate(scale_factor = 2, mode='nearest'),
             # nn.ReflectionPad2d(1),
             nn.Conv2d(output_dim * 4, output_dim * 2, kernel_size=3, stride=1, padding=1, bias=True),
             nn.BatchNorm2d(output_dim * 2),
             nn.ReLU(True),
             # state size. (output_dim*2) x 16 x 16
-            nn.Upsample(scale_factor = 2, mode='bilinear'),
+            nn.functional.interpolate(scale_factor = 2, mode='nearest'),
             # nn.ReflectionPad2d(1),
             nn.Conv2d(output_dim * 2,     output_dim, kernel_size=3, stride=1, padding=1, bias=True),
             nn.BatchNorm2d(output_dim),
             nn.ReLU(True),
             # state size. (output_dim) x 32 x 32
-            nn.Upsample(scale_factor = 2, mode='bilinear'),
+            nn.functional.interpolate(scale_factor = 2, mode='nearest'),
             # nn.ReflectionPad2d(1),
             nn.Conv2d(    output_dim,      1, kernel_size=3, stride=1, padding=1, bias=True),
             nn.Tanh()
