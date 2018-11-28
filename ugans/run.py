9ddd9321e4d9bd1e6a00f69255e1200d5dfedf61
Ian Gemp <imgemp@gmail.com>
2018-11-28 14:37:38 -0500

    forgot to update run.py

--- ugans/run.py
+++ ugans/run.py
@@ -104,6 +104,9 @@ def parse_params():
     elif args['domain'] == 'circles_small2':
         from examples.domains.circles_small2 import Circles as Domain
         from examples.domains.circles_small2 import Generator, AttExtractor, LatExtractor, Discriminator, Disentangler
+    elif args['domain'] == 'circles_small3':
+        from examples.domains.circles_small3 import Circles as Domain
+        from examples.domains.circles_small3 import Generator, AttExtractor, LatExtractor, Discriminator, Disentangler
     else:
         raise NotImplementedError(args['domain'])
 
