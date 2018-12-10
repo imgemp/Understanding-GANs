59958a2fe4a50fe4bd78790ac7b42a88f2d2da3d
Ian Gemp <imgemp@gmail.com>
2018-12-09 21:09:28 -0500

    patching bug

--- ugans/utils.py
+++ ugans/utils.py
@@ -88,9 +88,25 @@ def unzip(filepath):
         zf.extractall(dirpath)
     os.remove(filepath)
 
+# https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
+# https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive/39225039#39225039
 def download_file_from_google_drive(id, destination):
-    # URL = "https://docs.google.com/uc?export=download"
-    URL = "https://drive.google.com/uc?export=download"
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+
+        return None
+
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+
+    URL = "https://docs.google.com/uc?export=download"
 
     session = requests.Session()
 
@@ -101,7 +117,7 @@ def download_file_from_google_drive(id, destination):
         params = { 'id' : id, 'confirm' : token }
         response = session.get(URL, params = params, stream = True)
 
-    save_response_content(response, destination)    
+    save_response_content(response, destination) 
 
 def get_confirm_token(response):
     for key, value in response.cookies.items():
