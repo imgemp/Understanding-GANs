65de3b8ade539281cce2f7275cd6afa04da661c9
Ian Gemp <imgemp@gmail.com>
2018-12-18 15:29:37 -0500

    save images to tensorboard in color

--- ugans/run.py
+++ ugans/run.py
@@ -226,7 +226,7 @@ def run_experiment(Train, Domain, Generator, AttExtractor, LatExtractor, Discrim
             if params['n_viz'] > 0:
                 samples = train.m.get_fake(params['n_viz'], params['z_dim']).cpu().data.numpy()
                 np.save(params['saveto']+'samples/'+str(i), samples)
-                logger.image_summary('images', samples.reshape(-1, params['x_dim'], params['x_dim']), i)
+                logger.image_summary('images', samples.reshape(-1, params['x_dim'], params['x_dim'], params['c_dim']), i)
             data.plot_current(train, params, i)
 
         if params['plot_every'] > 0 and i % params['plot_every'] == 0:
