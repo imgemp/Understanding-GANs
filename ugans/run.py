a7be6140fd402dd262a5ebbe117950168e2ceed4
Ian Gemp <imgemp@gmail.com>
2019-01-07 13:11:15 -0500

    debugging logger

--- ugans/run.py
+++ ugans/run.py
@@ -229,7 +229,7 @@ def run_experiment(Train, Domain, Generator, AttExtractor, LatExtractor, Discrim
             if params['n_viz'] > 0:
                 samples = train.m.get_fake(params['n_viz'], params['z_dim']).cpu().data.numpy()
                 np.save(params['saveto']+'samples/'+str(i), samples)
-                logger_images = samples.reshape(-1, params['c_dim'], params['x_dim'], params['x_dim']).transpose((0,2,3,1))
+                logger_images = samples.reshape(-1, params['c_dim'], params['x_dim'], params['x_dim']).transpose((0,2,3,1)).squeeze()
                 logger.image_summary('images', logger_images, i)
             data.plot_current(train, params, i)
 
