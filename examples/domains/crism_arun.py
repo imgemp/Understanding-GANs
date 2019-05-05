300e7fbf52be626e1348a9df480b0c0f2099f307
Ian Gemp <imgemp@gmail.com>
2019-05-05 12:22:47 +0100

    debugging weight initialization

--- examples/domains/crism_arun.py
+++ examples/domains/crism_arun.py
@@ -6,12 +6,12 @@ from ugans.core import Data, Net
 
 
 def weights_init(m):
-        # classname = m.__class__.__name__
-        # if classname.find('Conv') != -1:
-        #     m.weight.data.normal_(0.0, 0.02)
-        # elif classname.find('BatchNorm') != -1:
-        #     m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(1.)
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            m.bias.data.fill_(1.)
+        elif classname.find('Linear') != -1:
+            m.bias.data.fill_(1.)
+        
 
 class View(nn.Module):
     def __init__(self, *shape):
