1abea32524d985eb38c5f1c23bb9ae6eb2fe664e
Ian Gemp <imgemp@gmail.com>
2019-05-21 18:34:11 +0100

    dataset strings are somewhat different

--- examples/domains/crism.py
+++ examples/domains/crism.py
@@ -24,8 +24,8 @@ from matplotlib.pyplot import cm
 import seaborn as sns
 
 # originals FRT000097E2, HRL000040FF
-directories = ['FRT000047A3', 'FRT00005850', 'FRT000097E2', 'FRT0000CBE5', 'FRT00013EBC', 'FRT000161EF', 'HRL000040FF', 'HRL0000C0BA']
-datasets = ['./examples/domains/data/CRISM_data_summPar_1/'+direc+'/'+direc+'_07_IF166L_TRR3_atcr_sabcondv4_1_Lib11123_1_4_5_l1_gadmm_a_v2_ca_ice_b200_MS' for direc in directories]
+ids = [('FRT000047A3','66'), ('FRT00005850','67'), ('FRT000097E2','66'), ('FRT0000CBE5','66'), ('FRT00013EBC','66'), ('FRT000161EF','67'), ('HRL000040FF','66'), ('HRL0000C0BA','83')]
+datasets = ['./examples/domains/data/CRISM_data_summPar_1/'+direc+'/'+direc+'_07_IF1'+num+'L_TRR3_atcr_sabcondv4_1_Lib11123_1_4_5_l1_gadmm_a_v2_ca_ice_b200_MS' for direc, num in ids]
 labelsets = [d+'_2014params' for d in datasets]
 
 # srun -p m40-long --gres=gpu:1 examples/run.sh "examples/args/crism/con/00.txt"
