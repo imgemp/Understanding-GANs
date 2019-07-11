9e666e1d61fbef930c5c2356ebfcee1e41b573c2
Ian Gemp <imgemp@gmail.com>
2019-07-10 21:09:37 -0600

    debugging

--- examples/domains/crism.py
+++ examples/domains/crism.py
@@ -308,6 +308,7 @@ class CRISM(Data):
         # matches = argmax cosine similarity
         matches = np.argmax(similarity, axis=1)
         # group spectra by matches
+        print(matches.shape)
         groups = {}
         for idx, match in enumerate(matches):
             if match not in groups:
@@ -317,7 +318,9 @@ class CRISM(Data):
         # for each class in generated spectra:
         # plot spectra and save plot with filename as mica match
         samples = samples.cpu().data.numpy()
+        print(groups)
         for endmember, sample_idxs in groups:
+            print(sample_idxs)
             group_samples = samples[list(sample_idxs)]
             plt.plot(self.waves, group_samples.T, 'r--')
             plt.plot(self.waves, self.mica_library[endmember], 'k-')
