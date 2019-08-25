d8a061853a14e9d8143666a9a68b693db42c4b1e
Ian Gemp <imgemp@gmail.com>
2019-08-25 22:15:51 +0100

    small fix

--- examples/domains/crism.py
+++ examples/domains/crism.py
@@ -367,7 +367,7 @@ class CRISM(Data):
         fig, ax = plt.subplots()
         n, bins, _ = plt.hist(matches, bins=len(self.mica_names), density=1, color='b', alpha=1.)
         ax.set_ylabel('counts')
-        ax.set_title('training endmember histogram ({:6.1f}k samples, {:d}/{:d} classes)'.format(matches.shape[0]/1000, num_matches, len(self.mica_names)))
+        ax.set_title('training endmember histogram ({:1.1f}k samples, {:d}/{:d} classes)'.format(matches.shape[0]/1000, num_matches, len(self.mica_names)))
         ax.set_xticks(np.arange(0,len(self.mica_names)))
         ax.set_xticklabels(self.mica_names, rotation=90)
         fig.tight_layout()
